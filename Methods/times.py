30deffaceeb2f4f7b343b3cc8839fd85c37ea39a
Ricardo López Cruz <230110088@itsoeh.edu.mx>
2024-09-18 22:02:29 -0600

    y

---
+++ Methods/times.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+def plot_times(runtime):
+
+    plt.figure(figsize=(15, 5)) 
+
+    plt.plot(range(1, len(runtime) + 1), runtime, 
+         linewidth=1.5, 
+         color="red",
+         linestyle="-", 
+         marker=">", 
+         markersize=10, 
+         markerfacecolor="blue", 
+         markeredgecolor="grey",
+         markeredgewidth=1) 
+
+    plt.xticks(runtime, fontsize=10)
+    plt.xlabel("Times", fontsize=15)
+    plt.ylabel("Runtime", fontsize=15)
+    plt.grid()
+    plt.show()
+    
